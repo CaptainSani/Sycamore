3fae97070dc682b87aebfb7d44bd80d3f9774ff6
Captain_Sani <sani.jdogo@gmail.com>
2024-09-18 09:31:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Sycamore mentorship program (Backend track)
+
+console.log("There is Git and there is GitHub");
+console.log("Git is a version control for tracking changes in code while Github is a platform which hosts git repositories and provides a freindly user interface for developers to use.");
+console.log("Git and GitHub has made it possible for developers to colaborate on projects from anywhere in the world");
+console.log("The git add <file name> is used to add a single file while the git add . is used to add all files in the project");
+console.log("The git push command is used to push your code to your branch before been merged to main and the git pull command is used to pull the most recent code from the repository into your local machine.")
